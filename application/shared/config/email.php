42cef49ab62d322396ece9dec3b373d62bb2c421
Jim <jimtrinidad002@gmail.com>
2018-12-28 13:54:37 +0800

    test

--- application/shared/config/email.php
+++ application/shared/config/email.php
@@ -1,12 +1,24 @@
 <?php
 
+// $config['email']['info'] = Array(
+// 	'useragent'	=> '',
+//     'protocol' 	=> 'smtp',
+//     'smtp_host' => 'mail.mgov.ph',
+//     'smtp_port' => 465,
+//     'smtp_user' => 'info@mgov.ph',
+//     'smtp_pass' => 'S%uE}_e@Sr}N',
+//     'mailtype'  => 'html', 
+//     'charset'   => 'iso-8859-1',
+//     'smtp_crypto'	=> 'ssl'
+// );
+
 $config['email']['info'] = Array(
 	'useragent'	=> '',
     'protocol' 	=> 'smtp',
-    'smtp_host' => 'mail.mgov.ph',
+    'smtp_host' => 'email-smtp.eu-west-1.amazonaws.com',
     'smtp_port' => 465,
-    'smtp_user' => 'info@mgov.ph',
-    'smtp_pass' => 'S%uE}_e@Sr}N',
+    'smtp_user' => 'AKIAJTGJXKMLJ4RTKCTQ',
+    'smtp_pass' => 'AuzCQhqptJBhUATJRI0H/ykrZqMBh70loR5DZpmd9+Cf',
     'mailtype'  => 'html', 
     'charset'   => 'iso-8859-1',
     'smtp_crypto'	=> 'ssl'
