0245aaa983972b5090408df70eed9dd4739bb9ed
Jim <jimtrinidad002@gmail.com>
2018-12-30 10:14:53 +0800

    j

---
+++ jobs/send_email_queue.php
@@ -0,0 +1,18 @@
+<?php
+
+$i = 1;
+while (true) {
+
+	// send queue
+	syslog(LOG_INFO, 'sending queued email.');
+	exec("php /var/www/mgovph/public/index.php adskfhasdfadsfasdfajgfkjsdf jasdfgkjgkjgasd");
+
+	// resend every 5 minutes
+	if ($i % 5 == 0) {
+		syslog(LOG_INFO, 'resending failed email.');
+		exec("php /var/www/mgovph/public/index.php adskfhasdfadsfasdfajgfkjsdf aldkslkjfgdsj");		
+	}
+
+	$i++;
+	sleep(60);
+}
