0c9138b7984ab30af8792c1e9a4a19c39d6f0a35
Jim <jimtrinidad002@gmail.com>
2019-01-07 21:54:40 +0800

    service debug

--- application/admin/views/pages/services/setupwizard.php
+++ application/admin/views/pages/services/setupwizard.php
@@ -370,7 +370,7 @@
                 });';
 
                 if ($serviceData->LocationScopeID == 6) {
-                    echo 'ServiceSetup.loadBarangayOptions("#LocationCode", "#citySelector", '. $serviceData->BarangayID .', function() {
+                    echo 'ServiceSetup.loadBarangayOptions("#LocationCode", "#citySelector", "'. $serviceData->BarangayID .'", function() {
                         ServiceSetup.setDepartmentSelector('. $serviceData->DepartmentLocationID .');
                         console.log("called");
                     });';
