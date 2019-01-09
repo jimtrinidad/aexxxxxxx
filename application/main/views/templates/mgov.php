145a2e43a5320b24a4b6ab4e4a36812523becb55
Jim <jimtrinidad002@gmail.com>
2019-01-09 20:51:07 +0800

    layout

--- application/main/views/templates/mgov.php
+++ application/main/views/templates/mgov.php
@@ -14,8 +14,8 @@
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" />
 
-		<link rel="stylesheet" href="<?php echo public_url(); ?>resources/css/style.css" rel="stylesheet">
-		<link rel="stylesheet" href="<?php echo public_url(); ?>resources/css/site.css" type="text/css"> 
+		<link rel="stylesheet" href="<?php echo public_url(); ?>resources/css/style.css?<?php echo time()?>" rel="stylesheet">
+		<link rel="stylesheet" href="<?php echo public_url(); ?>resources/css/site.css?<?php echo time()?>" type="text/css"> 
 
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
