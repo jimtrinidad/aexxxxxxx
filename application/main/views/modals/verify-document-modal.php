023826de454328a96c16b39765eec3a0b989892e
Jim <jimtrinidad002@gmail.com>
2020-07-26 02:07:35 +0800

    qr test

--- application/main/views/modals/verify-document-modal.php
+++ application/main/views/modals/verify-document-modal.php
@@ -18,67 +18,92 @@
 	</div>
 </div>
 
-<script type="text/javascript" src="<?php echo public_url(); ?>resources/libraries/html5-qrcode.min.js"></script>
+<script type="text/javascript" src="<?php echo public_url(); ?>resources/libraries/qrcode.min.js"></script>
+<script type="text/javascript" src="<?php echo public_url(); ?>resources/libraries/html5-qrcode.js"></script>
 <script type="text/javascript">
 	$(document).ready(function(){
-		const html5QrCode = new Html5Qrcode("qr-reader");
 
-        $('#verifyDocModal').on('show.bs.modal', function (e) {
-        	// $('#verifyDocModal').LoadingOverlay('show');
-        }).on('shown.bs.modal', function (e) {
-            // This method will trigger user permissions
-            Html5Qrcode.getCameras().then(devices => {
-              /**
-               * devices would be an array of objects of type:
-               * { id: "id", label: "label" }
-               */
-                console.log(devices);
-              if (devices && devices.length) {
+		function onScanSuccess(qrMessage) {
+			// handle the scanned code as you like
+			console.log(`QR matched = ${qrMessage}`);
+		}
 
-                var camera      = devices[0];
-                var cameraID    = camera.id
-                var cameraLabel = camera.label;
-                // .. use this to start scanning.
+		function onScanFailure(error) {
+			// handle scan failure, usually better to ignore and keep scanning
+			// console.warn(`QR error = ${error}`);
+		}
 
-                html5QrCode.start(
-                  cameraID,     // retreived in the previous step.
-                  {
-                    fps: 10,    // sets the framerate to 10 frame per second
-                    qrbox: 250  // sets only 250 X 250 region of viewfinder to
-                                // scannable, rest shaded.
-                  },
-                  qrCodeMessage => {
-                    // do something when code is read. For example:
-                    console.log(`QR Code detected: ${qrCodeMessage}`);
-                  },
-                  errorMessage => {
-                    // parse error, ideally ignore it. For example:
-                    // console.log(`QR Code no longer in front of camera.`);
-                  })
-                .catch(err => {
-                  // Start failed, handle it. For example,
-                  console.log(`Unable to start scanning, error: ${err}`);
-                });
+		let html5QrcodeScanner = new Html5QrcodeScanner("qr-reader",{ 
+			fps: 10, 
+			qrbox: 250 
+		}, false);
 
-                // $('#verifyDocModal').LoadingOverlay('hide');
 
-              }
+		$('#verifyDocModal').on('show.bs.modal', function (e) {
+			html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+		}).on('hide.bs.modal', function (e) {
+			html5QrcodeScanner.clear();
+		});
 
-            }).catch(err => {
-              bootbox.alert('Unable to find camera to use for scanning.')
-            });
+		// const html5QrCode = new Html5Qrcode("qr-reader");
 
-        }).on('hide.bs.modal', function (e) {
-            console.log('close');
-            html5QrCode.stop().then(ignore => {
-              // QR Code scanning is stopped.
-              console.log("QR Code scanning stopped.");
-            }).catch(err => {
-              // Stop failed, handle it.
-              console.log("Unable to stop scanning." + err);
-            });
+  //       $('#verifyDocModal').on('show.bs.modal', function (e) {
+  //       	// $('#verifyDocModal').LoadingOverlay('show');
+  //       }).on('shown.bs.modal', function (e) {
+  //           // This method will trigger user permissions
+  //           Html5Qrcode.getCameras().then(devices => {
+  //             /**
+  //              * devices would be an array of objects of type:
+  //              * { id: "id", label: "label" }
+  //              */
+  //               console.log(devices);
+  //             if (devices && devices.length) {
 
-            $('#verifyDocModal #qr-reader').html()
-        });
+  //               var camera      = devices[0];
+  //               var cameraID    = camera.id
+  //               var cameraLabel = camera.label;
+  //               // .. use this to start scanning.
+
+  //               html5QrCode.start(
+  //                 cameraID,     // retreived in the previous step.
+  //                 {
+  //                   fps: 10,    // sets the framerate to 10 frame per second
+  //                   qrbox: 250  // sets only 250 X 250 region of viewfinder to
+  //                               // scannable, rest shaded.
+  //                 },
+  //                 qrCodeMessage => {
+  //                   // do something when code is read. For example:
+  //                   console.log(`QR Code detected: ${qrCodeMessage}`);
+  //                 },
+  //                 errorMessage => {
+  //                   // parse error, ideally ignore it. For example:
+  //                   // console.log(`QR Code no longer in front of camera.`);
+  //                 })
+  //               .catch(err => {
+  //                 // Start failed, handle it. For example,
+  //                 console.log(`Unable to start scanning, error: ${err}`);
+  //               });
+
+  //               // $('#verifyDocModal').LoadingOverlay('hide');
+
+  //             }
+
+  //           }).catch(err => {
+  //             bootbox.alert('Unable to find camera to use for scanning.')
+  //           });
+
+  //       }).on('hide.bs.modal', function (e) {
+  //           console.log('close');
+  //           html5QrCode.stop().then(ignore => {
+  //             // QR Code scanning is stopped.
+  //             console.log("QR Code scanning stopped.");
+  //             html5QrCode.clear();
+  //           }).catch(err => {
+  //             // Stop failed, handle it.
+  //             console.log("Unable to stop scanning." + err);
+  //           });
+
+  //           $('#verifyDocModal #qr-reader').html()
+  //       });
 	})
 </script>
